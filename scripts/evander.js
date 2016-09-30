3e6fa741c2c74166151db239eaafdbc6acbaa6aa
Evander Batson <evanderbatson@gmail.com>
2016-09-30 18:12:21 -0400

    updates updates updates

--- scripts/evander.js
+++ scripts/evander.js
@@ -73,3 +73,20 @@ $("img").error(function () {
     $(this).hide();
     $(this).css({visibility:"hidden"}); 
 });
+
+//smooth scroll
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
